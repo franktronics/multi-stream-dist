13d57fbe42e3bae1943ec089fc77fdccb5d8ae10
franktronics <franktronicsprog@gmail.com>
2023-02-01 18:49:38 +0100

    somes test

--- apps/server/src/socketControllers/streamInit.controller.ts
+++ apps/server/src/socketControllers/streamInit.controller.ts
@@ -1,7 +1,12 @@
-import { Socket } from "socket.io";
+import { Socket } from "socket.io"
+import NodeMediaServer from 'node-media-server'
+import { nmsConfig } from "../config/nms.config"
 
 export const streamInit = (socket: Socket) => {
   socket.on("get_source_params", (arg) => {
     //find server params
+    const nms = new NodeMediaServer(nmsConfig)
+    nms.run()
+
   })
 }
